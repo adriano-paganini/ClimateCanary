8cc52813e40f44339c5591fd0a8d0a7acf7f522d
Prahbdip Singh <prahbdip.singh@student.uibk.ac.at>
2026-04-16 08:46:18 +0200

    relationship definition for SensorStation and getters/setters

--- src/main/java/at/qe/skeleton/sensorstation/model/SensorStation.java
+++ src/main/java/at/qe/skeleton/sensorstation/model/SensorStation.java
@@ -1,6 +1,7 @@
 package at.qe.skeleton.sensorstation.model;
 
 import at.qe.skeleton.raspberrypi.model.DeviceStatus;
+import at.qe.skeleton.raspberrypi.model.RaspberryPi;
 import jakarta.persistence.*;
 
 @Table(name = "sensorstations")
@@ -12,7 +13,49 @@ public class SensorStation {
     private Long id;
 
     private String name;
+
+    @Enumerated(EnumType.STRING)
     private DeviceStatus deviceStatus;
+
     private Float measurementsPerSec;
 
+    @ManyToOne
+    @JoinColumn(name = "raspberry_pi_id", nullable = false)
+    private RaspberryPi raspberryPi;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public DeviceStatus getDeviceStatus() {
+        return deviceStatus;
+    }
+
+    public void setDeviceStatus(DeviceStatus deviceStatus) {
+        this.deviceStatus = deviceStatus;
+    }
+
+    public Float getMeasurementsPerSec() {
+        return measurementsPerSec;
+    }
+
+    public void setMeasurementsPerSec(Float measurementsPerSec) {
+        this.measurementsPerSec = measurementsPerSec;
+    }
+
+    public RaspberryPi getRaspberryPi() {
+        return raspberryPi;
+    }
+
+    public void setRaspberryPi(RaspberryPi raspberryPi) {
+        this.raspberryPi = raspberryPi;
+    }
 }
