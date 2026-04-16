1e26c6553f45ae2fe3c5fe383a9c5c310a7f9350
Prahbdip Singh <prahbdip.singh@student.uibk.ac.at>
2026-04-16 08:43:21 +0200

    relationship definition for raspberry and getters/setters

--- src/main/java/at/qe/skeleton/raspberrypi/model/RaspberryPi.java
+++ src/main/java/at/qe/skeleton/raspberrypi/model/RaspberryPi.java
@@ -1,6 +1,10 @@
 package at.qe.skeleton.raspberrypi.model;
 
+import at.qe.skeleton.sensorstation.model.SensorStation;
 import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Table(name = "raspberrypis")
 @Entity
@@ -16,8 +20,43 @@ public class RaspberryPi {
     @Enumerated(EnumType.STRING)
     private DeviceStatus deviceStatus;
 
+    @OneToMany(mappedBy = "raspberryPi")
+    private List<SensorStation> sensorStations = new ArrayList<>();
 
+    public Long getId() {
+        return id;
+    }
 
+    public String getHostName() {
+        return hostName;
+    }
 
+    public void setHostName(String hostName) {
+        this.hostName = hostName;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    public DeviceStatus getDeviceStatus() {
+        return deviceStatus;
+    }
+
+    public void setDeviceStatus(DeviceStatus deviceStatus) {
+        this.deviceStatus = deviceStatus;
+    }
+
+    public List<SensorStation> getSensorStations() {
+        return sensorStations;
+    }
+
+    public void setSensorStations(List<SensorStation> sensorStations) {
+        this.sensorStations = sensorStations;
+    }
 }
 
