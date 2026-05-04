d78b375d551fa244aba48add399ad2616cba97a6
Adriano Paganini <adriano.paganini@student.uibk.ac.at>
2026-05-04 22:31:45 +0200

    feat: added automatic privacy mode updates for relevant raspberry Pis all 5 Minutes

--- src/main/java/at/qe/skeleton/scheduled/PrivacyModePeriodicUpdater.java
+++ src/main/java/at/qe/skeleton/scheduled/PrivacyModePeriodicUpdater.java
@@ -1,10 +1,9 @@
 package at.qe.skeleton.scheduled;
 
-import at.qe.skeleton.models.Absence;
-import at.qe.skeleton.models.Room;
-import at.qe.skeleton.models.RoomType;
-import at.qe.skeleton.models.Userx;
+import at.qe.skeleton.models.*;
 import at.qe.skeleton.services.AbsenceService;
+import at.qe.skeleton.services.EmployeeProfileService;
+import at.qe.skeleton.services.RaspberryPiServerService;
 import at.qe.skeleton.services.RoomService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -12,16 +11,21 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Component
 public class PrivacyModePeriodicUpdater {
 
     private final AbsenceService absenceService;
     private final RoomService roomService;
+    private final EmployeeProfileService employeeProfileService;
+    private final RaspberryPiServerService raspberryPiServerService;
 
-    public PrivacyModePeriodicUpdater(AbsenceService absenceService, RoomService roomService) {
+    public PrivacyModePeriodicUpdater(AbsenceService absenceService, RoomService roomService, EmployeeProfileService employeeProfileService, RaspberryPiServerService raspberryPiServerService) {
         this.absenceService = absenceService;
         this.roomService = roomService;
+        this.employeeProfileService = employeeProfileService;
+        this.raspberryPiServerService = raspberryPiServerService;
     }
 
     @Scheduled(cron = "0 */5 * * * *")
@@ -32,7 +36,22 @@ public class PrivacyModePeriodicUpdater {
         Set<Room> roomsWithAbsences = roomService.getRoomsByUserIds(allCurrentAbsences.stream().map(Absence::getUser).map(Userx::getId).toList());
         for (Room room : roomsWithAbsences) {
             if (room.getRoomType() == RoomType.OFFICE){
-                //TODO:implement logic, to determine rooms, with occupation less than 5
+                List<EmployeeProfile> assignedEmployees =  employeeProfileService.getAll(room.getId(),null);
+                Set<Long> absentUserIds = allCurrentAbsences.stream()
+                        .map(Absence::getUser)
+                        .map(Userx::getId)
+                        .collect(Collectors.toSet());
+
+                long occupancyCount = assignedEmployees.stream()
+                        .map(EmployeeProfile::getUser)
+                        .map(Userx::getId)
+                        .filter(userId -> !absentUserIds.contains(userId))
+                        .count();
+                if (room.getPrivacyMode() && occupancyCount >= 5){
+                    raspberryPiServerService.setOccupancy(room.getRaspberryPi().getId(),room.getId(),false);
+                }else if (!room.getPrivacyMode() && occupancyCount < 5){
+                    raspberryPiServerService.setOccupancy(room.getRaspberryPi().getId(),room.getId(),true);
+                }
             }
         }
     }
