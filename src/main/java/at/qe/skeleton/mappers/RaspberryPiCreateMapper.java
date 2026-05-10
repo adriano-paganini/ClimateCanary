38e534d3a38e92e2ed293459c27959c204ceb4a8
Adriano Paganini <adriano.paganini@student.uibk.ac.at>
2026-05-10 11:52:16 +0200

    changes: reverted changes, because they were not needed and incorrect

--- src/main/java/at/qe/skeleton/mappers/RaspberryPiCreateMapper.java
+++ src/main/java/at/qe/skeleton/mappers/RaspberryPiCreateMapper.java
@@ -3,23 +3,26 @@ package at.qe.skeleton.mappers;
 import at.qe.skeleton.common.DTOMapper;
 import at.qe.skeleton.dtos.RaspberryPiCreateDTO;
 import at.qe.skeleton.models.RaspberryPi;
-import at.qe.skeleton.models.Room;
+import at.qe.skeleton.services.RoomService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RaspberryPiCreateMapper implements DTOMapper<RaspberryPi, RaspberryPiCreateDTO> {
 
+    private final RoomService roomService;
 
+    public RaspberryPiCreateMapper(RoomService roomService) {
+        this.roomService = roomService;
+    }
 
     @Override
     public RaspberryPi mapFrom(RaspberryPiCreateDTO dto) {
         RaspberryPi pi = new RaspberryPi();
-        Room room = new Room();
-        room.setId(dto.roomId());
-        pi.setRoom(room);
+        pi.setRoom(roomService.getById(dto.roomId()));
         pi.setHostName(dto.hostName());
         return pi;
     }
+
 
     @Override
     public RaspberryPiCreateDTO mapTo(RaspberryPi entity) {
