0dcc1bf7f643793d802c49ac3bc9d2805f3af1aa
Adriano Paganini <adriano.paganini@student.uibk.ac.at>
2026-05-11 13:14:30 +0200

    hotfix: fixed scheduled method http timeout

--- src/main/java/at/qe/skeleton/configs/RestClientConfig.java
+++ src/main/java/at/qe/skeleton/configs/RestClientConfig.java
@@ -2,12 +2,22 @@ package at.qe.skeleton.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
+
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
 
     @Bean
-    public org.springframework.web.client.RestClient restClient() {
-        return org.springframework.web.client.RestClient.builder().build();
+    public RestClient restClient() {
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(Duration.ofSeconds(3));
+        requestFactory.setReadTimeout(Duration.ofSeconds(5));
+
+        return RestClient.builder()
+                .requestFactory(requestFactory)
+                .build();
     }
 }
