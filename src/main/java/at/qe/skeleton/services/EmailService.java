267be795a2f6d38e215257ef14c801da5a449a9e
Prahbdip Singh <prahbdip.singh@student.uibk.ac.at>
2026-05-26 14:03:43 +0200

    added email abstraction interface and EmailServiceImpl

--- src/main/java/at/qe/skeleton/services/EmailService.java
+++ src/main/java/at/qe/skeleton/services/EmailService.java
@@ -1,4 +1,8 @@
 package at.qe.skeleton.services;
 
+import at.qe.skeleton.models.Userx;
+
 public interface EmailService {
+
+    void sendUserInvitationEmail(Userx user);
 }
--- src/main/java/at/qe/skeleton/services/EmailServiceImpl.java
+++ src/main/java/at/qe/skeleton/services/EmailServiceImpl.java
@@ -1,4 +1,19 @@
 package at.qe.skeleton.services;
 
-public class EmailServiceImpl {
+import at.qe.skeleton.models.Userx;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class EmailServiceImpl implements EmailService {
+
+    @PreAuthorize("hasAuthority('SYSTEM_ADMIN')")
+    @Override
+    public void sendUserInvitationEmail(Userx user) {
+
+    }
 }
