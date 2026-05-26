39ce822d9f94e7389893c266e1beb8a313f22698
Prahbdip Singh <prahbdip.singh@student.uibk.ac.at>
2026-05-26 15:16:42 +0200

    template changes

--- src/main/java/at/qe/skeleton/services/EmailTemplateBuilder.java
+++ src/main/java/at/qe/skeleton/services/EmailTemplateBuilder.java
@@ -6,54 +6,78 @@ import org.springframework.stereotype.Component;
 @Component
 public class EmailTemplateBuilder {
 
-    private String buildFooter(){
+    private String wrap(String body) {
+        return """
+                <div style="font-family: Arial, sans-serif; line-height: 1.6;">
+                    %s
+                    %s
+                </div>
+                """.formatted(body, buildFooter());
+    }
+
+    private String buildFooter() {
         return """
                 <hr>
 
-                    <p style="font-size: 13px; color: gray;">
-                        This is an automatically generated email. Please do not reply.
-                    </p>
+                <p style="font-size: 13px; color: gray;">
+                    This is an automatically generated email. Please do not reply.
+                </p>
 
-                    <p>
-                        Kind regards,<br>
-                        <strong>G5T4 - Software Engineering</strong><br>
-                        University of Innsbruck
-                    </p>
+                <p>
+                    Kind regards,<br>
+                    <strong>G5T4 - Software Engineering</strong><br>
+                    University of Innsbruck
+                </p>
 
-                    <hr>
+                <hr>
 
-                    <p style="font-size: 11px; color: #777;">
-                        DISCLAIMER: This is a mock email created solely for a University 
-                        Software Engineering project at the University of Innsbruck. 
-                        This email and its contents do not represent a real company, 
-                        organization, or commercial entity.
-                    </p>
+                <p style="font-size: 11px; color: #777;">
+                    DISCLAIMER: This is a mock email created solely for a University 
+                    Software Engineering project at the University of Innsbruck. 
+                    This email and its contents do not represent a real company, 
+                    organization, or commercial entity.
+                </p>
                 """;
     }
 
     public String buildInvitationEmail(Userx user) {
 
-        return """
-                <div style="font-family: Arial, sans-serif; line-height: 1.6;">
-                
-                    <h2>ClimateCanary Invitation</h2>
+        String body = """
+                <h2>ClimateCanary Invitation</h2>
 
-                    <p>Hello <strong>%s</strong>,</p>
+                <p>Hello <strong>%s</strong>,</p>
 
-                    <p>You have been invited to join <strong>ClimateCanary</strong>.</p>
+                <p>You have been invited to join <strong>ClimateCanary</strong>.</p>
 
-                    <p>Your account has been created by an administrator.</p>
+                <p>Your account has been created by an administrator.</p>
 
-                    <p>Please use the following link to access the platform:</p>
+                <p>Please use the following link to access the platform:</p>
 
-                    <p>
-                        <a href="<Link>">...<Link></a>
-                    </p>
-                    
-                    %s
-         
-                </div>
-                """.formatted(user.getUsername(), buildFooter());
+                <p>
+                    <a href="<Link>"><Link></a>
+                </p>
+                """.formatted(user.getUsername());
+
+        return wrap(body);
+    }
+
+    public String buildAccountDeletedEmail(Userx user) {
+
+        String body = """
+                <h2>ClimateCanary Account Notice</h2>
+
+                <p>Hello <strong>%s</strong>,</p>
+
+                <p>
+                    This email is to inform you that your ClimateCanary account
+                    has been <strong>deleted</strong> by an administrator.
+                </p>
+
+                <p>
+                    If you believe this was done in error, please contact your system administrator.
+                </p>
+                """.formatted(user.getUsername());
+
+        return wrap(body);
     }
 }
-
